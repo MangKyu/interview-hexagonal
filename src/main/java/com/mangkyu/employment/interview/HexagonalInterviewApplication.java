b8db342155712934aeb507562603308648388f43
MangKyu <whalsrb1226@gmail.com>
2022-05-11 21:47:54 +0900

    Initial Commit

---
+++ src/main/java/com/mangkyu/employment/interview/HexagonalInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.mangkyu.employment.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mangkyu/employment/interview/HexagonalInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangkyu.employment.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

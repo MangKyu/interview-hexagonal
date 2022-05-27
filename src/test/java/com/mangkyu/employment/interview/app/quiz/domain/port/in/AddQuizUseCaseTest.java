2da01093c58bffddf81ddb3a1cdbddf34b265cb2
MangKyu <whalsrb1226@gmail.com>
2022-05-27 17:14:22 +0900

    퀴즈 추가 테스트 리팩토링

--- src/test/java/com/mangkyu/employment/interview/app/quiz/domain/port/in/AddQuizUseCaseTest.java
+++ src/test/java/com/mangkyu/employment/interview/app/quiz/domain/port/in/AddQuizUseCaseTest.java
@@ -1,26 +1,30 @@
-package com.mangkyu.employment.interview.app.quiz.application;
+package com.mangkyu.employment.interview.app.quiz.domain.port.in;
 
 import com.mangkyu.employment.interview.app.quiz.adapter.persistence.QuizEntity;
+import com.mangkyu.employment.interview.app.quiz.application.QuizService;
+import com.mangkyu.employment.interview.app.quiz.domain.port.out.LoadQuizPort;
 import com.mangkyu.employment.interview.app.quiz.domain.port.out.SaveQuizPort;
 import com.mangkyu.employment.interview.app.quiz.testbase.QuizTestBase;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AddQuizUseCaseTest {
 
-    @InjectMocks
-    private QuizService target;
+    private AddQuizUseCase target;
 
-    @Spy
     private SaveQuizPort saveQuizPort;
+
+    @BeforeEach
+    void init() {
+        this.saveQuizPort = spy(SaveQuizPort.class);
+        target = new QuizService(saveQuizPort, mock(LoadQuizPort.class));
+    }
 
     @Test
     void 퀴즈추가() {

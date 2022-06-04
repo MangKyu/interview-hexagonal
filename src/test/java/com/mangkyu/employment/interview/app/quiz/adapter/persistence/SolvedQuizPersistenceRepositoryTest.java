c6158fbbfb7acf48f16d7b0221b95f359f6fd991
MangKyu <whalsrb1226@gmail.com>
2022-06-04 23:46:09 +0900

    불필요한 테스트 제거

--- src/test/java/com/mangkyu/employment/interview/app/quiz/adapter/persistence/SolvedQuizPersistenceRepositoryTest.java
+++ src/test/java/com/mangkyu/employment/interview/app/quiz/adapter/persistence/SolvedQuizPersistenceRepositoryTest.java
@@ -1,20 +1,12 @@
 package com.mangkyu.employment.interview.app.quiz.adapter.persistence;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 class SolvedQuizPersistenceRepositoryTest {
 
     @Autowired
     private SolvedQuizPersistenceRepository target;
-
-    @Test
-    void temp() {
-        assertThat(target).isNotNull();
-    }
 
 }

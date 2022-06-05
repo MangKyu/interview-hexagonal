67e5e2854a53e95f10734e9002936535e029370b
MangKyu <whalsrb1226@gmail.com>
2022-06-06 00:22:42 +0900

    퀴즈 발송 크론잡 구현

---
+++ src/main/java/com/mangkyu/employment/interview/app/quiz/adapter/cron/SendQuizCronJob.java
@@ -0,0 +1,22 @@
+package com.mangkyu.employment.interview.app.quiz.adapter.cron;
+
+import com.mangkyu.employment.interview.app.quiz.domain.port.in.SendQuizUseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SendQuizCronJob {
+
+    private final SendQuizUseCase sendQuizUseCase;
+
+    @Scheduled(cron = "0 1 0 * * ?")
+    public void sendQuizMail() {
+        final int result = sendQuizUseCase.sendToEveryMember();
+        log.info("Send count: {}", result);
+    }
+
+}

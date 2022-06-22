b25c9d1a98557460a174569eb91bba3bc4ad0183
MangKyu <whalsrb1226@gmail.com>
2022-06-22 19:41:59 +0900

    SendQuizCronJob 인터페이스로 추상화

---
+++ src/main/java/com/mangkyu/employment/interview/app/quiz/adapter/cron/RealSendQuizCronJob.java
@@ -0,0 +1,24 @@
+package com.mangkyu.employment.interview.app.quiz.adapter.cron;
+
+import com.mangkyu.employment.interview.app.quiz.domain.port.in.SendQuizUseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Profile("!local")
+@Slf4j
+public class RealSendQuizCronJob implements SendQuizCronJob {
+
+    private final SendQuizUseCase sendQuizUseCase;
+
+    @Scheduled(cron = "0 1 0 * * ?")
+    public void sendQuizMail() {
+        final int result = sendQuizUseCase.sendToEveryMember();
+        log.info("Send count: {}", result);
+    }
+
+}
--- src/main/java/com/mangkyu/employment/interview/app/quiz/adapter/cron/SendQuizCronJob.java
+++ src/main/java/com/mangkyu/employment/interview/app/quiz/adapter/cron/SendQuizCronJob.java
@@ -1,24 +1,7 @@
 package com.mangkyu.employment.interview.app.quiz.adapter.cron;
 
-import com.mangkyu.employment.interview.app.quiz.domain.port.in.SendQuizUseCase;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+public interface SendQuizCronJob {
 
-@Component
-@RequiredArgsConstructor
-@Profile("!local")
-@Slf4j
-public class SendQuizCronJob {
-
-    private final SendQuizUseCase sendQuizUseCase;
-
-    @Scheduled(cron = "0 1 0 * * ?")
-    public void sendQuizMail() {
-        final int result = sendQuizUseCase.sendToEveryMember();
-        log.info("Send count: {}", result);
-    }
+    void sendQuizMail();
 
 }

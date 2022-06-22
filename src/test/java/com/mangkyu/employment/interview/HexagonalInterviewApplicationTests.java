91d2aaccdf28a4553f40980d4df4e4414a668e29
MangKyu <whalsrb1226@gmail.com>
2022-06-22 21:42:08 +0900

    테스트 코드 수정

--- src/test/java/com/mangkyu/employment/interview/HexagonalInterviewApplicationTests.java
+++ src/test/java/com/mangkyu/employment/interview/HexagonalInterviewApplicationTests.java
@@ -2,8 +2,10 @@ package com.mangkyu.employment.interview;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class HexagonalInterviewApplicationTests {
 
 	@Test
